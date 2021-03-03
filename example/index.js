00d2017939c1377dd5c8fa2d0a56ca90556fb438
mrsandmansy <star_rencai@163.com>
2021-03-03 10:02:52 +0800

    (+)version1

---
+++ example/index.js
@@ -0,0 +1,6 @@
+
+'use strict'
+
+import JsUtils from '../index'
+
+console.log(JsUtils.isPhone('15240295105'))
---
+++ index.js
@@ -0,0 +1,6 @@
+
+'use strict'
+
+import * as JsUtils from './lib/index'
+
+export default JsUtils
